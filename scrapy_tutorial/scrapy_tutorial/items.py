1bb1fe44fb3a7b8b7677a7156f5f5515a809adf7
zach.escalante <zach.escalante@gmail.com>
2018-10-02 21:43:00 -0400

    Latest webscraping notes added

--- scrapy_tutorial/scrapy_tutorial/items.py
+++ scrapy_tutorial/scrapy_tutorial/items.py
@@ -12,3 +12,4 @@ class ScrapyTutorialItem(scrapy.Item):
     # define the fields for your item here like:
     item = scrapy.Field()
     pass
+/Users/zacharyescalante/Library/Saved Searches/.DockTags/Red.tag6.savedSearch
--- scrapy_tutorial/scrapy_tutorial/spiders/CFEMScraper.py
+++ scrapy_tutorial/scrapy_tutorial/spiders/CFEMScraper.py
@@ -3,10 +3,13 @@ import io
 import json
 import scrapy
 import pandas as pd
-from scrapy_tutorial.items import ScrapyTutorialItem
-from scrapy.http import TextResponse 
-from selenium import webdriver
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from scrapy.http import TextResponse
+from scrapy_tutorial.items import ScrapyTutorialItem
+ 
+
+
 
 
 class CFEMScraper(scrapy.Spider):
@@ -16,8 +19,12 @@ class CFEMScraper(scrapy.Spider):
     #Start to scrape this/these url/urls
     start_urls = ['https://www.target.com/c/shop-all-categories/-/N-5xsxf']
 
+    #If we choose to use the "parse_selenium" function, uncomment this code to start 
+    #open an instance of Firefox (or your choice of webrowser) when the program begins
+    #####################COMMENT IN WHEN RUNNING "PARSE_SELENIUM"##################
     def __init__(self):
         self.driver = webdriver.Firefox()
+    ###############################################################################
 
     #The parsing function begins the scraping process
     def parse(self, response):
@@ -28,18 +35,24 @@ class CFEMScraper(scrapy.Spider):
             #callback makes a request to the "parse_link" HTML parsing function
             #2. I add the 'meta' tag to the scrapy request. This is a method of passing data to the next 
             #parsing function and saving it for future use/parsing
-            yield scrapy.Request(url=response.urljoin(link), callback=self.parse_xpath, meta={'webpage':response.urljoin(link)})
-    #Our first function 
+            yield scrapy.Request(url=response.urljoin(link), callback=self.parse_selenium, meta={'webpage':response.urljoin(link)})
+    
+    #Our first function "parse_xpath" uses traditional xpath in order to find the relevant objects on the webpage
     def parse_xpath(self, response):
+
+        #This simply prints the URL so that you know the correct links are being pulled
     	print("##################")
     	print(response.url)
     	print("##################")
         #Declare a custom object imported from scrapy_tutorial.items
         data = ScrapyTutorialItem()
         data['item'] = {'url': response.meta['webpage'].split("=")[1], 'items': response.xpath("//div[@class='ItemTitle-sc-1bls9ac-0 hrhyAs']/text()").extract()}
-        return data
+        return data 
 
+    #Our second function "parse_selenium" leverages the selenium webrowser to crawl our websites
     def parse_selenium(self, response):
+
+        #Use the previous instance of the webrowser which was created to go to visit the "response.url"
         self.driver.get(response.url)
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         #All comments have been loaded, once again pass the "body" argument back in
