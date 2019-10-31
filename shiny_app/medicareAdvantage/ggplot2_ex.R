fc5efbd71de1e52c404cbd1bfa215e57816096c6
zachescalante <zach.escalante@gmail.com>
2019-10-31 16:11:22 -0400

    Adding the ggplot2 graphs for time series data as an example file

--- shiny_app/medicareAdvantage/ggplot2_ex.R
+++ shiny_app/medicareAdvantage/ggplot2_ex.R
@@ -1,5 +1,6 @@
 #install.packages("ggplot2")
 
+library(dplyr)
 library(ggplot2)
 library(reshape2)
 
@@ -17,3 +18,20 @@ colnames(df)
 head(df)
 
 ggplot(df, aes(Var2, value, group=factor(Var1))) + geom_line(aes(color=factor(Var1)))
+
+#### Implement on real data ####
+
+county.data <- read.csv('county_payer_stats_t12mo.csv', header=TRUE, sep = ",")
+
+county.df <- county.data %>%
+  filter(State == "AL" & FIPS.State.County.Code == "1001") %>% # filter based on state and FIPS code
+  arrange(desc(Sep.19)) %>%                                    # sort based on last months' values
+  select (-c(X, State, FIPS.State.County.Code))                # Make sure to drop unnecessary columns
+
+# Take the top 10 
+county.df <- head(county.df, 10)
+
+# Create ggplot2 graph
+county.df <- melt(county.df, "Parent.Organization")
+ggplot(county.df, aes(variable, value, group = Parent.Organization, color = Parent.Organization)) +
+  geom_line()
